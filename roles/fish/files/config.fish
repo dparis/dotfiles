13bd0f2b7cff2964ca88d8cfe618f6aa3c64db49
Dylan Paris <dylan.paris@dylan.paris>
2020-06-15 22:42:08 -0700

    Install programming tools

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,1 +1,7 @@
+# Configure starship prompt
+# https://starship.rs/guide/
 starship init fish | source
+
+# Configure jenv
+set PATH $HOME/.jenv/bin $PATH
+status --is-interactive; and source (jenv init -|psub)
