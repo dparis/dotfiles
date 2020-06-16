1ec4134888c1beb8babd6c698be57984e2614765
Dylan Paris <dylan.paris@dylan.paris>
2020-06-16 00:23:13 -0700

    Use fnm instead of nvm

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -3,5 +3,10 @@
 starship init fish | source
 
 # Configure jenv
+# https://github.com/jenv/jenv#11-installing-jenv
 set PATH $HOME/.jenv/bin $PATH
 status --is-interactive; and source (jenv init -|psub)
+
+# Configure fnm
+# https://github.com/Schniz/fnm#manually
+fnm env --multi | source
